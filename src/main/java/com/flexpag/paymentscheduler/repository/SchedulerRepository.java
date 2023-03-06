e598e4f5921d576728c2352115b208ab5518bdb2
Marcelo Valada <marcelovalada2@hotmail.com>
2023-03-05 21:19:43 -0300

    Created SchedulerRepository

---
+++ src/main/java/com/flexpag/paymentscheduler/repository/SchedulerRepository.java
@@ -0,0 +1,8 @@
+package com.flexpag.paymentscheduler.repository;
+
+import com.flexpag.paymentscheduler.model.Scheduler;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SchedulerRepository extends JpaRepository<Scheduler, Long> {
+
+}
