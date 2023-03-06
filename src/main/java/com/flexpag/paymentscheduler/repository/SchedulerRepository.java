9933afae6dbc6ecf89246c2b2a3fcf3a635fabac
Marcelo Valada <marcelovalada2@hotmail.com>
2023-03-05 21:49:06 -0300

    Insert data into Bill e CreditCard tables

--- src/main/java/com/flexpag/paymentscheduler/repository/SchedulerRepository.java
+++ src/main/java/com/flexpag/paymentscheduler/repository/SchedulerRepository.java
@@ -1,11 +1,9 @@
 package com.flexpag.paymentscheduler.repository;
 
 import com.flexpag.paymentscheduler.model.Scheduler;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SchedulerRepository extends JpaRepository<Scheduler, Long> {
 
-    Page<Scheduler> findByStatusPaid(Pageable paging);
+
 }
